e60b04a0b96e63fa12434fc76f75c909d5cc82ee
Hasan Bayer <95939003+HasanBayer@users.noreply.github.com>
2023-02-11 21:08:41 +0300

    Add files via upload

---
+++ DataAccessLayer/EntityFramework/EfBlogRepository.cs
@@ -0,0 +1,29 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfBlogRepository : GenericRepository<Blog>, IBlogDal
+    {
+        public object GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetListWithCategory()
+        {
+            using (var c = new Context())
+            {
+                return c.Blogs.Include(x => x.Category).ToList();
+            }
+        }
+    }
+}
