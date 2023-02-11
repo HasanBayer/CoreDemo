7421cdabe2cd4070c52b728747aaddfebf5a393f
Hasan Bayer <95939003+HasanBayer@users.noreply.github.com>
2023-02-11 21:05:07 +0300

    Add files via upload

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("İsim boş geçilemez")
+                .MinimumLength(2).WithMessage("En az 2 karakter olmalı")
+                .MaximumLength(20).WithMessage("En fazla 20 karakter olabilir");
+
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail boş geçilemez")
+                .Matches(@"[@]+").WithMessage("Geçerli bir mail adresi girniz");
+            
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre Boş Geçilemez")
+                .MinimumLength(8).WithMessage("Şifre 8 karakterden küçük olamaz.")
+                .MaximumLength(16).WithMessage("Şifre 16 karakterden büyük olamaz.")
+                .Matches(@"[A-Z]+").WithMessage("Şifrede en az bir büyük harf olmalıdır.")
+                .Matches(@"[a-z]+").WithMessage("Şifrede en az bir küçük harf olmalıdır.")
+                .Matches(@"[0-9]+").WithMessage("Şifrede en az bir rakam olmalıdır");
+        }
+
+    }
+}
